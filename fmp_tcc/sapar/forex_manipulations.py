8c9a841e0ea7d04ef5b8bed843b6f5a65074a8f9
Bayan2019 <saparbayevabt@gmail.com>
2023-10-05 10:51:00 -0700

    fixing the bug

--- fmp_tcc/sapar/forex_manipulations.py
+++ fmp_tcc/sapar/forex_manipulations.py
@@ -3,7 +3,7 @@ import numpy as np
 from datetime import datetime
 
 # from get_forex_data import get_exchange_rate
-from get_collection_fs import company_profile, get_fs_some
+from .get_collection_fs import company_profile, get_fs_some
 
 def get_exchange_rate(apikey: str='apikey', exchange: str='EURUSD', period: str='daily',\
     start_date: str= "2023-10-01", end_date: str='2023-10-04'):
