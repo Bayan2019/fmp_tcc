a5f3399bda4267738e02fa651d8b0bfbea8827c8
Bayan2019 <saparbayevabt@gmail.com>
2023-10-29 16:50:15 -0700

    incrementing the version for PyPi

--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/Bayan2019/fmp_tcc',
-    version='0.1.21',
+    version='0.1.22',
     zip_safe=False,
 )
