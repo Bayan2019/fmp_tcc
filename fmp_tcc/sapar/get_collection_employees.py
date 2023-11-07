8595cf4e5b6174da2d995d89e7a2d619e170168b
Bayan <saparbayevabt@gmail.com>
2023-11-07 10:09:08 -0800

    text data

--- fmp_tcc/sapar/get_collection_employees.py
+++ fmp_tcc/sapar/get_collection_employees.py
@@ -7,6 +7,15 @@ from .get_info import get_employees_number
 def get_employees_counts(apikey: str='apikey', ticks: list=['AAPL', 'MSFT'], period: str = 'annual', 
            with_progress: bool = False):
     
+    """
+    params:
+        apikey -- to access data from FMP;
+        ticks -- list of tickers;
+        period -- either 'annual' or 'quarter';
+        with_progress -- if you want to visualize progress;
+    return: data frame of employees number
+    """
+    
     if with_progress:
         n = len(ticks)
         i=0
